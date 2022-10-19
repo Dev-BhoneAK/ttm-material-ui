693ac70f92792a576e5cc04591fd9bc0633b58f8
BhoneAK <bhoneak@outlook.com>
2022-10-19 23:26:24 +0630

    feat:add index

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import App from "./App";
+import theme from "./theme";
+import { BrowserRouter } from "react-router-dom";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
