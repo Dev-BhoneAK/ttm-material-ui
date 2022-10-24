bdd2315f8d388a5d30f16bc069c7df73f65fb0d6
BhoneAK <bhoneak@outlook.com>
2022-10-24 23:42:04 +0630

    feat:add Category Paper

--- src/components/common/CategoryPaper.js
+++ src/components/common/CategoryPaper.js
@@ -1,20 +1,39 @@
 import * as React from "react";
 import Paper from "@mui/material/Paper";
 import Icon from "@mui/material/Icon";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 export default function CategoryPaper(props) {
   return (
-    <Paper
-      elevation={3}
-      sx={{
-        width: { xs: 50, md: 80 },
-        height: { xs: 50, md: 80 },
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
+    <Stack
+      spacing={1}
+      alignItems="center"
+      sx={{ marginRight: props.name === "news" && { xs: 3, md: 5 } }}
     >
-      <Icon sx={{ fontSize: { xs: 20, md: 30 } }}>{props.icon}</Icon>
-    </Paper>
+      <Paper
+        elevation={3}
+        onClick={() => props.setActiveSlide(props.index)}
+        sx={{
+          width: { xs: 50, md: 80 },
+          height: { xs: 50, md: 80 },
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Icon
+          sx={{
+            fontSize: { xs: 20, md: 30 },
+            color: props.index === props.activeSlide && "blue",
+          }}
+        >
+          {props.icon}
+        </Icon>
+      </Paper>
+      <Typography color={props.index === props.activeSlide && "blue"}>
+        {props.title}
+      </Typography>
+    </Stack>
   );
 }
