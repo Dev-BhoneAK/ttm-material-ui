a91bcd9c79d2f8b63d46dd01cd5e8692887a69c6
BhoneAK <bhoneak@outlook.com>
2022-10-26 23:22:01 +0630

    feat:add Articles Section

---
+++ src/components/home/Articles.js
@@ -0,0 +1,27 @@
+import React from "react";
+import SectionTitle from "../common/SectionTitle";
+import ArticleIcon from "@mui/icons-material/Article";
+import MediaCard from "../common/MediaCard";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+export default function Articles() {
+  const settings = {
+    className: "slider variable-width",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    variableWidth: true,
+  };
+  return (
+    <>
+      <SectionTitle icon={<ArticleIcon />} title="Articles" />
+      <Slider {...settings}>
+        <MediaCard type="horizontal" margin="true" />
+        <MediaCard type="horizontal" margin="true" />
+        <MediaCard type="horizontal" margin="true" />
+      </Slider>
+    </>
+  );
+}
