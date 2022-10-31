73ed4b163fa3cc2ae4c60ef3cd2dbbe03c906142
BhoneAK <bhoneak@outlook.com>
2022-10-31 23:37:35 +0630

    fix: change color of heading section

--- src/components/common/SectionTitle.js
+++ src/components/common/SectionTitle.js
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import React from "react";
+import { grey } from "@mui/material/colors";
 
 export default function SectionTitle(props) {
   return (
@@ -19,10 +20,13 @@ export default function SectionTitle(props) {
           display: "flex",
           alignItems: "center",
           justifyContent: "space-between",
+          color: grey[800],
         }}
       >
         {props.icon}
-        <Typography variant="h6">{props.title}</Typography>
+        <Typography variant="h6" sx={{ ml: "5px" }}>
+          {props.title}
+        </Typography>
       </Box>
       <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
         Show All
