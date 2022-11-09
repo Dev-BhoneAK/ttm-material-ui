fa19cafc7710045fb6f4f1b5eef86fe85c58d123
BhoneAK <bhoneak@outlook.com>
2022-11-09 23:41:48 +0630

    feat: add News Article Detail screen

---
+++ src/components/news-articles-detail/ImageAppBar.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Container, Box } from "@mui/material";
+import Image from "mui-image";
+
+import DetailAppBar from "../app-bar/DetailAppBar";
+
+export default function ImageAppBar(props) {
+  console.log("props", props);
+  return (
+    <Box sx={{ position: "relative" }}>
+      <DetailAppBar />
+      <Image
+        src={`http://localhost:8000/assets${props?.image}`}
+        width="100%"
+        showLoading
+        duration={3000}
+        easing="ease-in-out"
+        bgColor="inherit"
+      />
+      <Container>
+        <Box>
+          {[...new Array(30)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+            )
+            .join("\n")}
+        </Box>
+      </Container>
+    </Box>
+  );
+}
