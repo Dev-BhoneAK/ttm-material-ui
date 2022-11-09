94853d721014293fa29eb234b140a0906e4f4f58
BhoneAK <bhoneak@outlook.com>
2022-11-09 23:40:55 +0630

    feat: add Custom Button

---
+++ src/components/common/CustomButton.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { grey, purple } from "@mui/material/colors";
+
+const CustomizedButton = styled(Button)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#222" : "#fff",
+  color: theme.palette.mode === "dark" ? "#fff" : grey[800],
+  "&:hover": {
+    backgroundColor: grey[500],
+    borderColor: "#0062cc",
+  },
+  "&:active": {
+    backgroundColor: grey[700],
+    borderColor: grey[800],
+  },
+  "&:focus": {
+    boxShadow: "0 0 0 0.2rem grey[900]",
+  },
+}));
+
+// const ColorButton = styled(Button)(({ theme }) => ({
+//   color: theme.palette.getContrastText(purple[500]),
+//   backgroundColor: purple[500],
+//   "&:hover": {
+//     backgroundColor: purple[700],
+//   },
+// }));
+
+export default function CustomButton() {
+  return (
+    <CustomizedButton
+      variant="contained"
+      fullWidth
+      endIcon={<NavigateNextIcon />}
+      disableRipple
+    >
+      View More
+    </CustomizedButton>
+  );
+}
