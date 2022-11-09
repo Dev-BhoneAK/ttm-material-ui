cc083cf24b823534caf7238a2f0648c616699391
BhoneAK <bhoneak@outlook.com>
2022-11-09 23:41:27 +0630

    feat: add Custom Typo

---
+++ src/components/common/CustomTypo.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { grey, purple } from "@mui/material/colors";
+
+export default function CustomTypo(props) {
+  const theme = useTheme();
+  return (
+    <Typography
+      variant={props.variant}
+      sx={{
+        // ml: "5px",
+        color: theme.palette.mode === "dark" ? "#c4c4c4" : grey[800],
+        ...props.styles,
+      }}
+    >
+      {props.title}
+    </Typography>
+  );
+}
