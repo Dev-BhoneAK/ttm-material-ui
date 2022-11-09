0974d5e7e5fb1db565070bd6743754f2ef4471d1
BhoneAK <bhoneak@outlook.com>
2022-11-09 23:41:06 +0630

    feat: add Custom Icon

---
+++ src/components/common/CustomIcon.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Icon from "@mui/material/Icon";
+
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { grey, purple } from "@mui/material/colors";
+
+const setColor = (props, theme) => {
+  let color = "";
+  if (props.index && props.index === props.activeSlide) {
+    color = "blue";
+  } else {
+    color = theme.palette.mode === "dark" ? "#fff" : grey[800];
+  }
+  return color;
+};
+
+export default function CustomIcon(props) {
+  const theme = useTheme();
+  return (
+    <Icon
+      sx={{
+        fontSize: { xs: "1.5rem", md: "2rem" },
+        color: setColor(props, theme),
+        ...props.styles,
+      }}
+    >
+      {props.icon}
+    </Icon>
+  );
+}
