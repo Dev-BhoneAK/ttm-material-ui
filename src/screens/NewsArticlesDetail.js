3cca139eef3293b43e820d66409e0e07e7d30f6f
BhoneAK <bhoneak@outlook.com>
2022-11-15 21:27:23 +0630

    feat: add more section on news detail

--- src/screens/NewsArticlesDetail.js
+++ src/screens/NewsArticlesDetail.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Container, Box } from "@mui/material";
+import { Container, Box, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { useParams } from "react-router-dom";
+import Divider from "@mui/material/Divider";
+import CustomTypo from "../components/common/CustomTypo";
 
 import { getNewsDetail } from "../utils/api";
 import ImageAppBar from "../components/news-articles-detail/ImageAppBar";
+import ContentInfo from "../components/news-articles-detail/ContentInfo";
 
 export default function NewsArticlesDetail() {
   const theme = useTheme();
@@ -14,14 +17,27 @@ export default function NewsArticlesDetail() {
   useEffect(() => {
     async function fetchData() {
       const newsDetail = await getNewsDetail(news_id);
-      console.log("newsDetail", newsDetail[0]);
-      setNewsDetailData(newsDetail[0]);
+
+      setNewsDetailData(newsDetail);
     }
     fetchData();
   }, []);
   return (
     <>
       <ImageAppBar image={newsDetailData?.image} />
+      <Container sx={{ px: { xs: 2 }, py: { xs: 2 } }}>
+        <CustomTypo variant="body1" title={newsDetailData?.title} />
+        {/* <Typography variant="h6">{newsDetailData?.title}</Typography> */}
+      </Container>
+      <Divider variant="middle" />
+      <ContentInfo
+        publishedDate={newsDetailData?.publishedDate}
+        viewCount={newsDetailData?.viewCount}
+      />
+      <Divider variant="middle" />
+      <Container sx={{ px: { xs: 2 }, py: { xs: 2 } }}>
+        <CustomTypo variant="body2" title={newsDetailData?.content} />
+      </Container>
     </>
   );
 }
