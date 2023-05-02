0813945b334568d06504c98b27606c3a06ccb1f2
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-02 23:47:33 +0630

    fix:change global font

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Home from "./screens/Home";
 import NewsArticlesDetail from "./screens/NewsArticlesDetail";
 import ColorModeContext from "./context/ColorModeContext";
 import { responsiveFontSizes } from "@mui/material/styles";
-import Typography from '@mui/material/Typography';
 
 export default function App() {
   const [mode, setMode] = React.useState("light");
@@ -21,33 +20,37 @@ export default function App() {
     []
   );
 
-  const theme = React.useMemo(
-    () => {
-        return createTheme({
-            palette: {
-                mode,
-            },
-        })
+  const theme = React.useMemo(() => {
+    return createTheme({
+      palette: {
+        mode,
+      },
+    });
+  }, [mode]);
+
+  theme.typography.body1 = {
+    fontSize: "0.87rem",
+    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1rem",
     },
-    [mode]
-  );
+  };
 
   const themeWithResponsiveFont = responsiveFontSizes(theme);
 
-    return (
+  return (
     <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={themeWithResponsiveFont}>
-          <CssBaseline />
-          <Suspense fallback={<div>Loading...</div>}>
-            <Container maxWidth="md" sx={{ px: { xs: 0 } }}>
-            <Typography variant="subtitle1">Responsive h3</Typography>
-              <Routes>
-                <Route path="/" exact index element={<Home />} />
-                <Route path="/news/:news_id" element={<NewsArticlesDetail />} />
-              </Routes>
-            </Container>
-          </Suspense>
-        </ThemeProvider>
+      <ThemeProvider theme={themeWithResponsiveFont}>
+        <CssBaseline />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Container maxWidth="md" sx={{ px: { xs: 0 } }}>
+            <Routes>
+              <Route path="/" exact index element={<Home />} />
+              <Route path="/news/:news_id" element={<NewsArticlesDetail />} />
+            </Routes>
+          </Container>
+        </Suspense>
+      </ThemeProvider>
     </ColorModeContext.Provider>
   );
 }
