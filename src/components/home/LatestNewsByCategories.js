d1cebc3f69d06aa9ac9d78c9c302a7b045c51190
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-03 23:51:56 +0630

    fix: wrap container to component

--- src/components/home/LatestNewsByCategories.js
+++ src/components/home/LatestNewsByCategories.js
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { Container } from "@mui/material";
 
 import MediaCard from "../common/MediaCard";
 import CustomizedButton from "../common/CustomButton";
 import SectionTitle from "../common/SectionTitle";
-import CustomLink from "../common/CustomLink"
+import CustomLink from "../common/CustomLink";
 import CustomIcon from "../common/CustomIcon";
 import { getLatestNewsByCategories } from "../../utils/api";
 
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
 export default function LatestNewsByCategories({ newsCategories }) {
-
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const [latestNews, setLatestNews] = useState([]);
 
@@ -28,70 +28,82 @@ export default function LatestNewsByCategories({ newsCategories }) {
   };
 
   useEffect(() => {
-    getLatestNewsByCategories(newsCategories[activeTabIndex]?.id).then((data) => {
-      setLatestNews(data);
-    });
-  
+    getLatestNewsByCategories(newsCategories[activeTabIndex]?.id).then(
+      (data) => {
+        setLatestNews(data);
+      }
+    );
   }, [activeTabIndex]);
 
   useEffect(() => {
-    
     getLatestNewsByCategories(1).then((data) => {
+      console.log(data);
       setLatestNews(data);
     });
-  
   }, []);
 
   return (
-    <>
+    <Container>
       <SectionTitle icon="newspaper" title="Latest News By Categories" />
-      <Tabs value={activeTabIndex}
+      <Tabs
+        value={activeTabIndex}
         onChange={handleChange}
         variant="scrollable"
         scrollButtons="auto"
-        aria-label="icon label tabs example">
-        {
-          newsCategories.map((newsCategory, index) => (
-            <Tab label={newsCategory.name} 
-            {...a11yProps(index)} 
-            icon={<CustomIcon icon={newsCategory.icon} styles={{fontSize: { xs: "1rem", md: "1.5rem" }, mb: 1}}/>} />
-          ))
-        }
+        aria-label="icon label tabs example"
+      >
+        {newsCategories.map((newsCategory, index) => (
+          <Tab
+            label={newsCategory.name}
+            {...a11yProps(index)}
+            icon={
+              <CustomIcon
+                icon={newsCategory.icon}
+                styles={{ fontSize: { xs: "1rem", md: "1.5rem" }, mb: 1 }}
+              />
+            }
+          />
+        ))}
       </Tabs>
       <Grid container spacing={2}>
-        {
-          latestNews.map((data, index) => (
-            index < 2 ? (
-
-              <Grid item xs={12} sm={6} sx={{ display: index === 1 ? { xs: 'none', sm: 'block' } : undefined }}>
-                <CustomLink to={`/news/${data?.id}`}>
-                  <MediaCard
-                    type="vertical"
-                    key={data?.id}
-                    data={data}
-                  />
-                </CustomLink>
-              </Grid>
-            ) : (
-
-              <Grid item xs={12} sm={6}>
-                <MediaCard
-                  type="horizontal"
-                  margin="false"
-                  key={data?.id}
-                  data={data}
-                />
-              </Grid>
-            ))
+        {latestNews.map((data, index) =>
+          index < 2 ? (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              sx={{
+                display: index === 1 ? { xs: "none", sm: "block" } : undefined,
+              }}
+            >
+              <CustomLink to={`/news/${data?.id}`}>
+                <MediaCard type="vertical" key={data?.id} data={data} />
+              </CustomLink>
+            </Grid>
+          ) : (
+            <Grid item xs={12} sm={6}>
+              <MediaCard
+                type="horizontal"
+                margin="false"
+                key={data?.id}
+                data={data}
+              />
+            </Grid>
           )
-        }
+        )}
       </Grid>
 
       <Grid container spacing={1}>
-        <Grid item xs={12} md={12} sx={{ mt: 1.5, display: 'flex' }} justifyContent="center">
+        <Grid
+          item
+          xs={12}
+          md={12}
+          sx={{ mt: 1.5, display: "flex" }}
+          justifyContent="center"
+        >
           <CustomizedButton />
         </Grid>
       </Grid>
-    </>
+    </Container>
   );
 }
