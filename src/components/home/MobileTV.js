1fcda6f2c137b20522104e3feddcae7361afbd25
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-03 23:53:01 +0630

    fix: wrap container to component

--- src/components/home/MobileTV.js
+++ src/components/home/MobileTV.js
@@ -4,6 +4,7 @@ import MediaCard from "../common/MediaCard";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { Container } from "@mui/material";
 
 export default function MobileTV({ videos }) {
   const settings = {
@@ -14,13 +15,13 @@ export default function MobileTV({ videos }) {
     variableWidth: true,
   };
   return (
-    <>
+    <Container>
       <SectionTitle icon="live_tv" title="Mobile TV" />
       <Slider {...settings}>
         {videos.map((video) => (
           <MediaCard type="video" margin="true" key={video.id} data={video} />
         ))}
       </Slider>
-    </>
+    </Container>
   );
 }
