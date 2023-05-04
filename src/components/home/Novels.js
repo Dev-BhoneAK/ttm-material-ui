4d27f24503e302e92374fed10ccc23ce96f20290
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-04 23:44:35 +0630

    fix: add section spacing in each section

--- src/components/home/Novels.js
+++ src/components/home/Novels.js
@@ -4,8 +4,9 @@ import MediaCard from "../common/MediaCard";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { Box } from "@mui/material";
 
-export default function Novels({ novels }) {
+export default function Novels({ novels, sectionSpacing }) {
   const settings = {
     className: "slider variable-width",
     slidesToShow: 1,
@@ -14,13 +15,13 @@ export default function Novels({ novels }) {
     variableWidth: true,
   };
   return (
-    <>
+    <Box sx={{ ...sectionSpacing }}>
       <SectionTitle icon="menu_book" title="Novels" />
       <Slider {...settings}>
         {novels.map((novel) => (
           <MediaCard type="image" margin="true" key={novel.id} data={novel} />
         ))}
       </Slider>
-    </>
+    </Box>
   );
 }
