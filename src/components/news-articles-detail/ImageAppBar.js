191139194e001bb5df49b251813a6c6de2e9437f
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-13 23:50:45 +0630

    fix: image app bar

--- src/components/news-articles-detail/ImageAppBar.js
+++ src/components/news-articles-detail/ImageAppBar.js
@@ -8,7 +8,17 @@ export default function ImageAppBar(props) {
   return (
     <>
       <Container sx={{ position: "relative", px: { xs: 0 } }}>
-        <DetailAppBar />
+        <DetailAppBar
+          styles={{
+            backgroundColor: "rgba(0,0,0,0.3)",
+            position: "fixed",
+            left: "0",
+            top: "0",
+            maxWidth: "md",
+            px: { xs: 0 },
+            margin: "0 auto",
+          }}
+        />
         <Image
           src={`${process.env.REACT_APP_API_DOMAIN}/assets${props?.image}`}
           width="100%"
