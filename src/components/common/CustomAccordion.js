3d74e523aeb3933b3a4e8a5dfbf8d90a6a460b94
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-13 23:50:06 +0630

    feat: add Custom Accordion

---
+++ src/components/common/CustomAccordion.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+export default function CustomAccordion({ title, detail, styles }) {
+  return (
+    <>
+      <Accordion sx={{ ...styles }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{detail}</Typography>
+        </AccordionDetails>
+      </Accordion>
+    </>
+  );
+}
