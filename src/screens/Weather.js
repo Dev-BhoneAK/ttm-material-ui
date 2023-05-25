b0a0c21676e585b0687eedd4f1e97259c3fb6460
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-25 21:40:06 +0630

    feat:add animation on weather card

--- src/screens/Weather.js
+++ src/screens/Weather.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
+import Grow from "@mui/material/Grow";
+
 import DetailAppBar from "../components/app-bar/DetailAppBar";
 import HeadingTitle from "../components/common/HeadingTitle";
 import { WeatherCard } from "../components/common/MediaCard";
@@ -17,11 +19,22 @@ export default function Weather() {
         styles={styles}
       />
       <Box sx={{ px: 3, pb: 3 }}>
-        <WeatherCard date="20-02-2022" icon="RAIN" />
-        <WeatherCard date="21-02-2022" icon="CLOUDY" />
-        <WeatherCard date="22-02-2022" icon="PARTLY_CLOUDY_DAY" />
-        <WeatherCard date="23-02-2022" icon="SLEET" />
-        <WeatherCard date="24-02-2022" icon="CLEAR_DAY" />
+        <Grow in timeout={1000}>
+          <WeatherCard date="20-02-2022" icon="RAIN" />
+        </Grow>
+
+        <Grow in timeout={1000} style={{ transitionDelay: "500ms" }}>
+          <WeatherCard date="21-02-2022" icon="CLOUDY" />
+        </Grow>
+        <Grow in timeout={1000} style={{ transitionDelay: "1000ms" }}>
+          <WeatherCard date="22-02-2022" icon="PARTLY_CLOUDY_DAY" />
+        </Grow>
+        <Grow in timeout={1000} style={{ transitionDelay: "1500ms" }}>
+          <WeatherCard date="23-02-2022" icon="SLEET" />
+        </Grow>
+        <Grow in timeout={1000} style={{ transitionDelay: "2000ms" }}>
+          <WeatherCard date="24-02-2022" icon="CLEAR_DAY" />
+        </Grow>
       </Box>
     </>
   );
