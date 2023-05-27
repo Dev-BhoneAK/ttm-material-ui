51929fc7b8fba6d58f85086f9534a7449d24a692
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-27 23:54:43 +0630

    delete: useGreyBackground custom hook

--- src/hooks/useGreyBackground.js
+++
@@ -1,9 +0,0 @@
-import { useTheme } from "@mui/material/styles";
-
-export default function useGreyBackground() {
-  const theme = useTheme();
-  //   const greyBackground = theme.palette.mode === "dark" ? "inherit" : "#e1e1e1";
-  const greyBackground = theme.palette.mode === "dark" ? "#1e1e1e" : "#e1e1e1";
-  //   const greyBackground = "#e1e1e1";
-  return greyBackground;
-}
