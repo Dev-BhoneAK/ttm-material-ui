0c209aa4db766d08a9bb71682b1613907a623c25
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-27 23:53:46 +0630

    fix:add dynamic data

--- src/screens/Weather.js
+++ src/screens/Weather.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Grow from "@mui/material/Grow";
+import Zoom from "@mui/material/Zoom";
 
 import DetailAppBar from "../components/app-bar/DetailAppBar";
 import HeadingTitle from "../components/common/HeadingTitle";
@@ -19,22 +20,57 @@ export default function Weather() {
         styles={styles}
       />
       <Box sx={{ px: 3, pb: 3 }}>
-        <Grow in timeout={1000}>
+        {/* <Zoom in timeout={1000}>
           <WeatherCard date="20-02-2022" icon="RAIN" />
-        </Grow>
+        </Zoom>
 
-        <Grow in timeout={1000} style={{ transitionDelay: "500ms" }}>
+        <Zoom in timeout={1000} style={{ transitionDelay: "500ms" }}>
           <WeatherCard date="21-02-2022" icon="CLOUDY" />
+        </Zoom> */}
+
+        <Zoom in timeout={2000}>
+          <WeatherCard date="22-02-2022" icon="PARTLY_CLOUDY_DAY" day="TODAY" />
+        </Zoom>
+        <Zoom in timeout={2000} style={{ transitionDelay: "500ms" }}>
+          <WeatherCard date="21-02-2022" icon="CLOUDY" day="MON" />
+        </Zoom>
+        <Zoom in timeout={2000} style={{ transitionDelay: "1000ms" }}>
+          <WeatherCard date="21-02-2022" icon="CLOUDY" day="TUE" />
+        </Zoom>
+        <Zoom in timeout={2000} style={{ transitionDelay: "1500ms" }}>
+          <WeatherCard date="23-02-2022" icon="SLEET" day="WED" />
+        </Zoom>
+        <Zoom in timeout={2000} style={{ transitionDelay: "2000ms" }}>
+          <WeatherCard date="24-02-2022" icon="CLEAR_DAY" day="THURS" />
+        </Zoom>
+        <Zoom in timeout={2000} style={{ transitionDelay: "2500ms" }}>
+          <WeatherCard date="24-02-2022" icon="CLEAR_DAY" day="FRI" />
+        </Zoom>
+        <Zoom in timeout={2000} style={{ transitionDelay: "3000ms" }}>
+          <WeatherCard date="24-02-2022" icon="CLEAR_DAY" day="SAT" />
+        </Zoom>
+
+        {/* <Grow in timeout={1000}>
+          <WeatherCard date="22-02-2022" icon="PARTLY_CLOUDY_DAY" day="TODAY" />
         </Grow>
-        <Grow in timeout={1000} style={{ transitionDelay: "1000ms" }}>
-          <WeatherCard date="22-02-2022" icon="PARTLY_CLOUDY_DAY" />
+        <Grow in timeout={1000} style={{ transitionDelay: "500ms" }}>
+          <WeatherCard date="21-02-2022" icon="CLOUDY" day="MON" />
         </Grow>
-        <Grow in timeout={1000} style={{ transitionDelay: "1500ms" }}>
-          <WeatherCard date="23-02-2022" icon="SLEET" />
+        <Grow in timeout={2000} style={{ transitionDelay: "1000ms" }}>
+          <WeatherCard date="21-02-2022" icon="CLOUDY" day="TUE" />
         </Grow>
-        <Grow in timeout={1000} style={{ transitionDelay: "2000ms" }}>
-          <WeatherCard date="24-02-2022" icon="CLEAR_DAY" />
+        <Grow in timeout={3000} style={{ transitionDelay: "1500ms" }}>
+          <WeatherCard date="23-02-2022" icon="SLEET" day="WED" />
         </Grow>
+        <Grow in timeout={4000} style={{ transitionDelay: "2000ms" }}>
+          <WeatherCard date="24-02-2022" icon="CLEAR_DAY" day="THURS" />
+        </Grow>
+        <Grow in timeout={5000} style={{ transitionDelay: "2500ms" }}>
+          <WeatherCard date="24-02-2022" icon="CLEAR_DAY" day="FRI" />
+        </Grow>
+        <Grow in timeout={6000} style={{ transitionDelay: "3000ms" }}>
+          <WeatherCard date="24-02-2022" icon="CLEAR_DAY" day="SAT" />
+        </Grow> */}
       </Box>
     </>
   );
