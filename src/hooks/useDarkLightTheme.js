b759d291932a635bdaae7cecb81925ee841040ad
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-27 23:53:30 +0630

    feat:add custom hook useDarkLightTheme

---
+++ src/hooks/useDarkLightTheme.js
@@ -0,0 +1,10 @@
+import { useTheme } from "@mui/material/styles";
+
+export default function useDarkLightTheme() {
+  const theme = useTheme();
+  const greyBackground = theme.palette.mode === "dark" ? "#1e1e1e" : "#e1e1e1";
+  const blueBox = theme.palette.mode === "dark" ? "#272727" : "#1876d1";
+  const whiteBox = theme.palette.mode === "dark" ? "#121212" : "#ffffff";
+  const textColor = theme.palette.mode === "dark" ? "#ffffff" : "#121212";
+  return { greyBackground, blueBox, whiteBox, textColor };
+}
