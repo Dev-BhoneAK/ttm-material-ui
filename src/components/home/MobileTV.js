7f210bbe860acb66bcea7d2468eac8055f747e29
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-28 23:28:56 +0630

    feat: split code to media-cards

--- src/components/home/MobileTV.js
+++ src/components/home/MobileTV.js
@@ -1,10 +1,11 @@
 import React from "react";
-import SectionTitle from "../common/SectionTitle";
-import MediaCard from "../common/MediaCard";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { Box } from "@mui/material";
+
+import SectionTitle from "../common/SectionTitle";
+import VideoMediaCard from "../common/meida-cards/VideoMediaCard";
 
 export default function MobileTV({ videos, sectionSpacing }) {
   const settings = {
@@ -16,10 +17,10 @@ export default function MobileTV({ videos, sectionSpacing }) {
   };
   return (
     <Box sx={{ ...sectionSpacing }}>
-      <SectionTitle icon="live_tv" title="Mobile TV" />
+      <SectionTitle icon="live_tv" title="Mobile TV" showAll />
       <Slider {...settings}>
         {videos.map((video) => (
-          <MediaCard type="video" margin="true" key={video.id} data={video} />
+          <VideoMediaCard margin="true" key={video.id} data={video} />
         ))}
       </Slider>
     </Box>
