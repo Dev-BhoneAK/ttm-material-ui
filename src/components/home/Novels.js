a5afb80cb58cf28f437f336f112e924ff75e76fe
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-30 14:35:59 +0630

    fix: novel section

--- src/components/home/Novels.js
+++ src/components/home/Novels.js
@@ -1,10 +1,11 @@
 import React from "react";
-import SectionTitle from "../common/SectionTitle";
-import MediaCard from "../common/MediaCard";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { Box } from "@mui/material";
+
+import SectionTitle from "../common/SectionTitle";
+import ImageMediaCard from "../common/meida-cards/ImageMediaCard";
 
 export default function Novels({ novels, sectionSpacing }) {
   const settings = {
@@ -13,13 +14,15 @@ export default function Novels({ novels, sectionSpacing }) {
     slidesToScroll: 1,
     arrows: false,
     variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
   };
   return (
     <Box sx={{ ...sectionSpacing }}>
       <SectionTitle icon="menu_book" title="Novels" />
       <Slider {...settings}>
         {novels.map((novel) => (
-          <MediaCard type="image" margin="true" key={novel.id} data={novel} />
+          <ImageMediaCard margin="true" key={novel.id} data={novel} />
         ))}
       </Slider>
     </Box>
