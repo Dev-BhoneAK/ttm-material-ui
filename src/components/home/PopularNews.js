06708daaa543bd0b0f72e03719b5ec02f87c4c32
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-05-30 14:36:42 +0630

    fix: popular section

--- src/components/home/PopularNews.js
+++ src/components/home/PopularNews.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import MediaCard from "../common/MediaCard";
 import { Box } from "@mui/material";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
+import useUpperTabletSize from "../../hooks/useUpperTabletSize";
 import SectionTitle from "../common/SectionTitle";
+import HorizontalMediaCard from "../common/meida-cards/HorizontalMediaCard";
 
 export default function PopularNews({ popularNews, sectionSpacing }) {
-  const theme = useTheme();
-  const upperTabletSize = useMediaQuery(theme.breakpoints.up("sm"));
+  const upperTabletSize = useUpperTabletSize();
   const settings = {
     className: "slider variable-width",
     slidesToShow: 1,
@@ -25,8 +23,7 @@ export default function PopularNews({ popularNews, sectionSpacing }) {
       <SectionTitle icon="trending_up" title="Popular News" />
       <Slider {...settings}>
         {popularNews.map((popularNew) => (
-          <MediaCard
-            type="horizontal"
+          <HorizontalMediaCard
             margin="true"
             key={popularNew.id}
             data={popularNew}
