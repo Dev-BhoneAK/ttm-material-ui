1833c10c6469a8809dc858f4058bd8d211987816
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-21 23:42:59 +0630

    feat: add News List

--- src/screens/NewsList.js
+++ src/screens/NewsList.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from "react";
+import Grid from "@mui/material/Grid";
 import { Box } from "@mui/material";
 
 import { sectionSpacing } from "../utils/commonStyle";
 import { getCategories } from "../utils/api";
 import MainAppBar from "../components/app-bar/MainAppBar";
 import SearchBox from "../components/search-box";
-import PopularCategories from "../components/home/PopularCategories";
 import BackgroundGrey from "../components/common/BackgroundGrey";
-import LatestNewsByCategories from "../components/home/LatestNewsByCategories";
 import BottomNavigationBar from "../components/app-bar/BottomNavigationBar";
+import NewsCategoriesList from "../components/common/NewsCategoriesList";
+import HorizontalMediaCard from "../components/common/meida-cards/HorizontalMediaCard";
+import VerticalMediaCard from "../components/common/meida-cards/VerticalMediaCard";
+import { getLatestNewsByCategories } from "../utils/api";
 
 export default function NewsList() {
   const [newsCategoriesData, setNewsCategoriesData] = useState([]);
+  const [latestNews, setLatestNews] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -20,18 +24,47 @@ export default function NewsList() {
     }
     fetchData();
   }, []);
+
+  useEffect(() => {
+    getLatestNewsByCategories(1).then((data) => {
+      setLatestNews(data);
+    });
+  }, []);
   return (
     <>
       <MainAppBar sectionSpacing={sectionSpacing} />
       <Box sx={{ p: "0.5rem 1.5rem" }}>
         <SearchBox sectionSpacing={sectionSpacing} />
-        <PopularCategories sectionSpacing={sectionSpacing} />
       </Box>
       <BackgroundGrey styles={sectionSpacing}>
-        <LatestNewsByCategories
-          newsCategories={newsCategoriesData}
-          sectionSpacing={sectionSpacing}
-        />
+        <Box sx={{ ...sectionSpacing }}>
+          <NewsCategoriesList
+            newsCategories={newsCategoriesData}
+            setLatestNews={setLatestNews}
+          />
+          <Grid container spacing={2}>
+            {latestNews.map((data, index) =>
+              index < 2 ? (
+                <Grid
+                  item
+                  xs={12}
+                  //   sm={6}
+                  key={data?.id}
+                  sx={{
+                    display:
+                      index === 1 ? { xs: "none", sm: "block" } : undefined,
+                  }}
+                >
+                  <VerticalMediaCard data={data} />
+                </Grid>
+              ) : (
+                <Grid item xs={12} sm={6} key={data?.id}>
+                  <HorizontalMediaCard margin="false" data={data} />
+                </Grid>
+              )
+            )}
+          </Grid>
+        </Box>
       </BackgroundGrey>
       <BottomNavigationBar />
     </>
