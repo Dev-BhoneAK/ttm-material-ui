deb1c5503a7a3596d39d17c16079b324e9cd41cc
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-21 23:42:46 +0630

    fix: change layout

--- src/components/home/LatestNewsByCategories.js
+++ src/components/home/LatestNewsByCategories.js
@@ -1,41 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
-import Tabs, { tabsClasses } from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
 import { Box } from "@mui/material";
 
+import NewsCategoriesList from "../common/NewsCategoriesList";
 import HorizontalMediaCard from "../common/meida-cards/HorizontalMediaCard";
 import VerticalMediaCard from "../common/meida-cards/VerticalMediaCard";
 import CustomizedButton from "../common/CustomButton";
 import SectionTitle from "../common/SectionTitle";
-import CustomIcon from "../common/CustomIcon";
 import { getLatestNewsByCategories } from "../../utils/api";
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
 
 export default function LatestNewsByCategories({
   newsCategories,
   sectionSpacing,
 }) {
-  const [activeTabIndex, setActiveTabIndex] = useState(0);
   const [latestNews, setLatestNews] = useState([]);
-
-  const handleChange = (event, newValue) => {
-    setActiveTabIndex(newValue);
-  };
-
-  useEffect(() => {
-    getLatestNewsByCategories(newsCategories[activeTabIndex]?.id).then(
-      (data) => {
-        setLatestNews(data);
-      }
-    );
-  }, [activeTabIndex]);
 
   useEffect(() => {
     getLatestNewsByCategories(1).then((data) => {
@@ -46,33 +24,7 @@ export default function LatestNewsByCategories({
   return (
     <Box sx={{ ...sectionSpacing }}>
       <SectionTitle icon="newspaper" title="Latest News By Categories" />
-      <Tabs
-        value={activeTabIndex}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons
-        aria-label="visible arrows tabs example"
-        sx={{
-          [`& .${tabsClasses.scrollButtons}`]: {
-            "&.Mui-disabled": { opacity: 0.3 },
-          },
-          mb: { xs: 1, sm: 1.5 },
-        }}
-      >
-        {newsCategories.map((newsCategory, index) => (
-          <Tab
-            label={newsCategory.name}
-            key={newsCategory?.id}
-            {...a11yProps(index)}
-            icon={
-              <CustomIcon
-                icon={newsCategory.icon}
-                styles={{ fontSize: { xs: "1rem", md: "1.5rem" }, mb: 1 }}
-              />
-            }
-          />
-        ))}
-      </Tabs>
+      <NewsCategoriesList newsCategories={newsCategories} setLatestNews={setLatestNews}/>
       <Grid container spacing={2}>
         {latestNews.map((data, index) =>
           index < 2 ? (
