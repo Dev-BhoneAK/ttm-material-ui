34f7c3d202062bb13d20a21a83fbec265894f57b
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-21 23:41:59 +0630

    feat: add Link for videos list

--- src/components/app-bar/BottomNavigationBar.js
+++ src/components/app-bar/BottomNavigationBar.js
@@ -11,7 +11,6 @@ import LiveTvIcon from "@mui/icons-material/LiveTv";
 import { Link, useLocation } from "react-router-dom";
 
 export default function BottomNavigationBar() {
-  const [value, setValue] = React.useState(0);
   const location = useLocation();
 
   return (
@@ -19,27 +18,35 @@ export default function BottomNavigationBar() {
       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
       elevation={3}
     >
-      <BottomNavigation
-        showLabels
-        value={location.pathname}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
+      <BottomNavigation showLabels value={location.pathname}>
         <BottomNavigationAction
           label="Home"
           icon={<HomeIcon />}
           component={Link}
           to="/"
+          value="/"
         />
         <BottomNavigationAction
           label="News"
           icon={<FeedIcon />}
           component={Link}
           to="/news"
+          value="/news"
         />
-        <BottomNavigationAction label="Novels" icon={<MenuBookIcon />} />
-        <BottomNavigationAction label="Mobile TV" icon={<LiveTvIcon />} />
+        <BottomNavigationAction
+          label="Novels"
+          icon={<MenuBookIcon />}
+          component={Link}
+          to="/novels"
+          value="/novels"
+        />
+        <BottomNavigationAction
+          label="Mobile TV"
+          icon={<LiveTvIcon />}
+          component={Link}
+          to="/videos"
+          value="/videos"
+        />
         <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
       </BottomNavigation>
     </Paper>
