0f92f21cd8f0927feff19381b7ecb87e5d69170d
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-06-22 23:54:37 +0630

    fix: use text over flow ellipsisi from commmon style

--- src/utils/commonStyle.js
+++ src/utils/commonStyle.js
@@ -1,3 +1,13 @@
 export const sectionSpacing = {
   mb: { xs: 2, sm: 3 },
 };
+
+export const textOverflowEllipsis = {
+  variant: "body1",
+  maxWidth: "100%",
+  display: "-webkit-box",
+  WebkitBoxOrient: "vertical",
+  WebkitLineClamp: 1,
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+};
