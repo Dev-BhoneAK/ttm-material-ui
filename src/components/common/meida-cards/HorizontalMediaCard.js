a2ddb1718074f4cbf67dd61876f24eab997be8f6
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-07-03 23:49:08 +0630

    fix: optimize code

--- src/components/common/meida-cards/HorizontalMediaCard.js
+++ src/components/common/meida-cards/HorizontalMediaCard.js
@@ -6,29 +6,28 @@ import Typography from "@mui/material/Typography";
 
 import { textOverflowEllipsis } from "../../../utils/commonStyle";
 
-
 export default function HorizontalMediaCard({ data, margin, width }) {
   return (
-      <Card
-        sx={{
-          mr: margin === "true" && 1.5,
-          width: width,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "left",
-        }}
-      >
-        <CardMedia
-          component="img"
-          sx={{ width: 100, mr: "8px" }}
-          image={`${process.env.REACT_APP_API_DOMAIN}/assets${data?.image}`}
-          alt={data?.title}
-        />
-        <CardContent sx={{ "&:last-child": { p: 0 } }}>
-          <Typography sx={{ ...textOverflowEllipsis }}>
-            {data?.title}
-          </Typography>
-        </CardContent>
-      </Card>
+    <Card
+      sx={{
+        mr: margin === "true" && 1.5,
+        width: width,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "left",
+      }}
+    >
+      <CardMedia
+        component="img"
+        sx={{ width: 100, mr: "8px" }}
+        image={`${process.env.REACT_APP_API_DOMAIN}/assets${data?.image}`}
+        alt={data?.title}
+      />
+      <CardContent sx={{ "&:last-child": { p: 0 } }}>
+        <Typography sx={{ ...textOverflowEllipsis }} variant="body1">
+          {data?.title}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
