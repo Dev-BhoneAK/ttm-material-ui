e457022e95ad2c8816504a5c6c50589c05db11de
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-11-21 23:55:47 +0630

    fix: update detail page content info

--- src/components/common/ContentInfo.js
+++ src/components/common/ContentInfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
+import IconButton from "@mui/material";
 
+import UserActionMenu from "./UserActionMenu";
 import CustomIcon from "./CustomIcon";
 import CustomTypo from "./CustomTypo";
 
@@ -8,6 +10,14 @@ export default function ContentInfo(props) {
   const iconStyles = {
     fontSize: { xs: "1.3rem", md: "1.8rem" },
     marginRight: { xs: "0.1rem", md: "0.2rem" },
+  };
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
@@ -25,7 +35,26 @@ export default function ContentInfo(props) {
         <CustomIcon icon="visibility" styles={iconStyles} />
         <CustomTypo variant="subtitle1" title={props?.viewCount} />
       </Stack>
-      <CustomIcon icon="star" styles={iconStyles} />
+      <IconButton
+        // aria-label="more"
+        // id="long-button"
+        // aria-controls={open ? "long-menu" : undefined}
+        // aria-expanded={open ? "true" : undefined}
+        // aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <CustomIcon icon="more_horiz" />
+      </IconButton>
+      <UserActionMenu open={open} handleClose={handleClose} />
+      {/* <IconButton
+        size="large"
+        edge="end"
+        aria-label="back-to-home"
+        color="inherit"
+        sx={{ pr: { xs: 0, sm: 1.5 } }}
+      >
+        <CustomIcon icon="more_horiz" />
+      </IconButton> */}
     </Stack>
   );
 }
