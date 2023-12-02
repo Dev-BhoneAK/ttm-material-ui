1eb1949a96f2488b12732e0fdcd2ddfcc9d6deea
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-12-02 23:57:28 +0630

    fix: separate Router Component

--- src/App.js
+++ src/App.js
@@ -1,73 +1,18 @@
 import * as React from "react";
 import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 
-import Home from "./screens/Home";
-import NewsList from "./screens/NewsList";
-import NovelsList from "./screens/NovelsList";
-import VideosList from "./screens/VideosList";
-import NewsArticlesDetail from "./screens/NewsArticlesDetail";
-import NovelDetail from "./screens/NovelDetail";
-import VideoDetail from "./screens/VideoDetail";
-import QuestionAnswer from "./screens/QuestionAnswer";
-import Weather from "./screens/Weather";
-import SearchResults from "./screens/SearchResults";
-import ColorModeContext from "./context/ColorModeContext";
-import { responsiveFontSizes } from "@mui/material/styles";
+import AppRoutes from "./AppRoutes";
+import AppTheme from "./AppTheme";
 
 export default function App() {
-  const [mode, setMode] = React.useState("light");
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-      },
-    }),
-    []
-  );
-
-  const theme = React.useMemo(() => {
-    return createTheme({
-      palette: {
-        mode,
-      },
-    });
-  }, [mode]);
-
-  theme.typography.body1 = {
-    fontSize: "0.87rem",
-    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-    [theme.breakpoints.up("sm")]: {
-      fontSize: "1rem",
-    },
-  };
-
-  const themeWithResponsiveFont = responsiveFontSizes(theme);
-
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={themeWithResponsiveFont}>
-        <CssBaseline />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Container maxWidth="md" sx={{ px: { xs: 0 } }}>
-            <Routes>
-              <Route path="/" exact index element={<Home />} />
-              <Route path="/news" element={<NewsList />} />
-              <Route path="/news/:news_id" element={<NewsArticlesDetail />} />
-              <Route path="/novels" element={<NovelsList />} />
-              <Route path="/novels/:novel_id" element={<NovelDetail />} />
-              <Route path="/videos" element={<VideosList />} />
-              <Route path="/videos/:video_id" element={<VideoDetail />} />
-              <Route path="/question-answer" element={<QuestionAnswer />} />
-              <Route path="/weather" element={<Weather />} />
-              <Route path="/search" element={<SearchResults />} />
-            </Routes>
-          </Container>
-        </Suspense>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <AppTheme>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Container maxWidth="md" sx={{ px: { xs: 0 } }}>
+          <AppRoutes />
+        </Container>
+      </Suspense>
+    </AppTheme>
   );
 }
