9bb56876b66d9e08c834b0dfd8fb8786971fbca6
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-12-02 23:55:43 +0630

    feat: add AppThemes

---
+++ src/AppTheme.js
@@ -0,0 +1,72 @@
+import { useState, useMemo } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ColorModeContext from "./context/ColorModeContext";
+import { responsiveFontSizes } from "@mui/material/styles";
+
+import useLocalStorage from "./hooks/useLocalStorage";
+
+function AppTheme(props) {
+  //   const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState(() => {
+    try {
+      // Attempt to get the stored value from local storage
+      const item = localStorage.getItem("darkMode");
+      // Parse the stored JSON or return the initialValue if it doesn't exist
+      console.log("item ", item);
+      if (item) {
+        return JSON.parse(item);
+      } else {
+        localStorage.setItem("darkMode", JSON.stringify("light"));
+        return "light";
+      }
+      //   return item ? JSON.parse(item) : "light";
+    } catch (error) {
+      console.error("Error retrieving data from local storage:", error);
+      return initialValue;
+    }
+  });
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        console.log("toogle mode ", mode);
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+        // const newMode = mode === "light" ? "dark" : "light";
+        // setMode(newMode);
+        localStorage.setItem("darkMode", JSON.stringify(mode));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(() => {
+    return createTheme({
+      palette: {
+        mode,
+      },
+    });
+  }, [mode]);
+
+  console.log("current mode ", mode);
+
+  theme.typography.body1 = {
+    fontSize: "0.87rem",
+    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1rem",
+    },
+  };
+
+  const themeWithResponsiveFont = responsiveFontSizes(theme);
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={themeWithResponsiveFont}>
+        <CssBaseline />
+        {props.children}
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default AppTheme;
