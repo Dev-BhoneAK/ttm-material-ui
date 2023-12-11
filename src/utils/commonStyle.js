69638ac7f0be38095d5714b8ed796bacd1bba5b0
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-12-11 20:31:22 +0630

    fix: utils

--- src/utils/commonStyle.js
+++ src/utils/commonStyle.js
@@ -1,28 +1,28 @@
 export const sectionSpacing = {
-  mb: { xs: 2, sm: 3 },
+    mb: { xs: 2, sm: 3 }
 };
 
 export const spaceFromNavigationBar = {
-  mb: 7,
+    mb: 7
 };
 
 export const textOverflowEllipsis = {
-  maxWidth: "100%",
-  display: "-webkit-box",
-  WebkitBoxOrient: "vertical",
-  WebkitLineClamp: 1,
-  overflow: "hidden",
-  textOverflow: "ellipsis",
+    maxWidth: '100%',
+    display: '-webkit-box',
+    WebkitBoxOrient: 'vertical',
+    WebkitLineClamp: 1,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis'
 };
 
 export const circularButton = {
-  borderRadius: "100%",
-  width: 35,
-  height: 35,
-  minWidth: 35,
-  minHeight: 35,
-  p: 0,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
+    borderRadius: '100%',
+    width: 35,
+    height: 35,
+    minWidth: 35,
+    minHeight: 35,
+    p: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
 };
