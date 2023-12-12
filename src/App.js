51f8ace92743f7ced6df869c3525a2677bccfa96
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-12-12 13:49:51 +0630

    fix: change app Theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import * as React from "react";
-import { Suspense } from "react";
-import Container from "@mui/material/Container";
+import * as React from 'react';
+import { Suspense } from 'react';
+import Container from '@mui/material/Container';
 
-import AppRoutes from "./AppRoutes";
-import AppTheme from "./AppTheme";
+import AppRoutes from './AppRoutes';
+import AppTheme from './AppTheme';
 
 export default function App() {
-  return (
-    <AppTheme>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Container maxWidth="md" sx={{ px: { xs: 0 } }}>
-          <AppRoutes />
-        </Container>
-      </Suspense>
-    </AppTheme>
-  );
+    return (
+        <AppTheme>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Container maxWidth="md" sx={{ px: { xs: 0 } }}>
+                    <AppRoutes />
+                </Container>
+            </Suspense>
+        </AppTheme>
+    );
 }
--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,31 +1,31 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
-import Home from "./screens/Home";
-import NewsList from "./screens/NewsList";
-import NovelsList from "./screens/NovelsList";
-import VideosList from "./screens/VideosList";
-import NewsArticlesDetail from "./screens/NewsArticlesDetail";
-import NovelDetail from "./screens/NovelDetail";
-import VideoDetail from "./screens/VideoDetail";
-import QuestionAnswer from "./screens/QuestionAnswer";
-import Weather from "./screens/Weather";
-import SearchResults from "./screens/SearchResults";
+import Home from './screens/Home';
+import NewsList from './screens/NewsList';
+import NovelsList from './screens/NovelsList';
+import VideosList from './screens/VideosList';
+import NewsArticlesDetail from './screens/NewsArticlesDetail';
+import NovelDetail from './screens/NovelDetail';
+import VideoDetail from './screens/VideoDetail';
+import QuestionAnswer from './screens/QuestionAnswer';
+import Weather from './screens/Weather';
+import SearchResults from './screens/SearchResults';
 
 function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" exact index element={<Home />} />
-      <Route path="/news" element={<NewsList />} />
-      <Route path="/news/:news_id" element={<NewsArticlesDetail />} />
-      <Route path="/novels" element={<NovelsList />} />
-      <Route path="/novels/:novel_id" element={<NovelDetail />} />
-      <Route path="/videos" element={<VideosList />} />
-      <Route path="/videos/:video_id" element={<VideoDetail />} />
-      <Route path="/question-answer" element={<QuestionAnswer />} />
-      <Route path="/weather" element={<Weather />} />
-      <Route path="/search" element={<SearchResults />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" exact index element={<Home />} />
+            <Route path="/news" element={<NewsList />} />
+            <Route path="/news/:news_id" element={<NewsArticlesDetail />} />
+            <Route path="/novels" element={<NovelsList />} />
+            <Route path="/novels/:novel_id" element={<NovelDetail />} />
+            <Route path="/videos" element={<VideosList />} />
+            <Route path="/videos/:video_id" element={<VideoDetail />} />
+            <Route path="/question-answer" element={<QuestionAnswer />} />
+            <Route path="/weather" element={<Weather />} />
+            <Route path="/search" element={<SearchResults />} />
+        </Routes>
+    );
 }
 
 export default AppRoutes;
