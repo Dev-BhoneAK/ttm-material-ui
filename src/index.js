391c11641c048e47ff00ce86382ec62eaa2bc163
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-12-12 13:55:09 +0630

    fix: change index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { pdfjs } from "react-pdf";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { pdfjs } from 'react-pdf';
 
-import App from "./App";
-import "./index.css";
+import App from './App';
+import './index.css';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
