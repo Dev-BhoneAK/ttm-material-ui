20e1c6b32520c42c3edb68103dc159a75bb710fa
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-12-25 23:50:45 +0630

    fix: add SharedStateProvider in Search Box

--- src/components/common/HeaderSection.js
+++ src/components/common/HeaderSection.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import PropTypes from 'prop-types';
 
 import { sectionSpacing } from '../../utils/commonStyle';
 import MainAppBar from '../app-bar/MainAppBar';
 import SearchBox from '../search-box';
-
-export default function HeaderSection() {
+import { SharedStateProvider } from '../../context/SearchDataContext';
+export default function HeaderSection({ children }) {
     return (
         <>
             <MainAppBar sectionSpacing={sectionSpacing} />
             <Box sx={{ p: 2 }}>
-                <SearchBox sectionSpacing={sectionSpacing} />
+                <SharedStateProvider>
+                    <SearchBox sectionSpacing={sectionSpacing} />
+                    {children}
+                </SharedStateProvider>
             </Box>
         </>
     );
 }
+
+HeaderSection.propTypes = {
+    children: PropTypes.node
+};
