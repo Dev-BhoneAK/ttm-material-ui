fe71af4ccfec4c79c1c30a08cd7b33a7dffffb57
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-12-26 23:51:21 +0630

    fix: NewsList

--- src/screens/NewsList.js
+++ src/screens/NewsList.js
@@ -2,63 +2,65 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@mui/material/Grid';
 import { Box } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
 
 import CustomLink from '../components/common/CustomLink';
-import { getCategories } from '../utils/api';
-import { sectionSpacing } from '../utils/commonStyle';
-import MainAppBar from '../components/app-bar/MainAppBar';
-import SearchBox from '../components/search-box';
+import { getCategories, getLatestNewsByCategories } from '../utils/api';
+import { sectionSpacing, spaceFromNavigationBar } from '../utils/commonStyle';
 import BackgroundGrey from '../components/common/BackgroundGrey';
-import BottomNavigationBar from '../components/app-bar/BottomNavigationBar';
 import NewsCategoriesList from '../components/common/NewsCategoriesList';
 import HorizontalMediaCard from '../components/common/meida-cards/HorizontalMediaCard';
 import VerticalMediaCard from '../components/common/meida-cards/VerticalMediaCard';
-import { getLatestNewsByCategories } from '../utils/api';
 import useUpperTabletSize from '../hooks/useUpperTabletSize';
 
 export default function NewsList() {
-    const [newsCategoriesData, setNewsCategoriesData] = useState([]);
-    const [latestNews, setLatestNews] = useState([]);
+    // const [newsCategoriesData, setNewsCategoriesData] = useState([]);
+    // const [latestNews, setLatestNews] = useState([]);
     const upperTabletSize = useUpperTabletSize();
+    const { data: newsCategories } = useQuery({
+        queryKey: ['categories', { parentCategory: 'news' }],
+        queryFn: getCategories
+    });
+    const localNews = newsCategories?.[0].id; // Because Placing local news at First Tab
 
-    useEffect(() => {
-        async function fetchData() {
-            const newsCategories = await getCategories('news');
-            setNewsCategoriesData(newsCategories);
-        }
-        fetchData();
-    }, []);
+    const { data: latestNews } = useQuery({
+        queryKey: ['news', { categoryId: 1 }],
+        queryFn: getLatestNewsByCategories,
+        enabled: !!localNews
+    });
 
-    useEffect(() => {
-        getLatestNewsByCategories(1).then((data) => {
-            setLatestNews(data);
-        });
-    }, []);
+    console.log('newssss', latestNews);
+
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         const newsCategories = await getCategories('news');
+    //         setNewsCategoriesData(newsCategories);
+    //     }
+    //     fetchData();
+    // }, []);
+
+    // useEffect(() => {
+    //     getLatestNewsByCategories(1).then((data) => {
+    //         setLatestNews(data);
+    //     });
+    // }, []);
     return (
         <>
-            <MainAppBar sectionSpacing={sectionSpacing} />
-            <Box sx={{ p: '0.5rem 1.5rem' }}>
-                <SearchBox sectionSpacing={sectionSpacing} />
-            </Box>
-            <BackgroundGrey styles={sectionSpacing}>
+            <BackgroundGrey styles={{ ...sectionSpacing, ...spaceFromNavigationBar }}>
                 <Box>
                     <NewsCategoriesList
-                        newsCategories={newsCategoriesData}
-                        setLatestNews={setLatestNews}
+                        newsCategories={newsCategories}
+                        // setLatestNews={setLatestNews}
                     />
-                    <Grid container spacing={2}>
-                        {/* {upperTabletSize
-              ? NewsListUpperTabletView({ latestNews })
-              : NewsListMobileView({ latestNews })} */}
+                    {/* <Grid container spacing={2}>
                         {upperTabletSize ? (
                             <NewsListUpperTabletView newsList={latestNews} />
                         ) : (
                             <NewsListMobileView newsList={latestNews} />
                         )}
-                    </Grid>
+                    </Grid> */}
                 </Box>
             </BackgroundGrey>
-            <BottomNavigationBar />
         </>
     );
 }
